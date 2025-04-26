8512530baa116e20efc1134ae26d6fc52b58f225
Kumaran S <kumaran.s@velammal.edu.in>
2025-04-27 01:00:54 +0530

    Added: Title color change changes: Copy function

--- web-passwordgenerator-project/index.js
+++ web-passwordgenerator-project/index.js
@@ -1,7 +1,8 @@
-//Random character sets
+// Random character SET
 const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\\";
 let copyButtonOne=document.getElementById("copy-btn-1")
 let copyButtonTwo=document.getElementById("copy-btn-2")
+
 // assin  op 1 to 1st space  // op2 to 2nd space
 let outputOne=document.getElementById("output-1")
 let outputTwo=document.getElementById("output-2")
@@ -11,44 +12,55 @@ let outputTwo=document.getElementById("output-2")
 function passWordGeneratorOne(){
     let passWordOne=""
     for (let i=0 ; i<15 ; i++){
-    let randomNumGenOne=Math.floor(Math.random()*characters.length)
-    let randNumOne=characters[randomNumGenOne]
-    passWordOne+=(randNumOne)
-}
-return passWordOne
+        let randomNumGenOne=Math.floor(Math.random()*characters.length)
+        let randNumOne=characters[randomNumGenOne]
+        // condition paswd shouldn't start with - according to IBM
+        while(randNumOne == '-'){
+            randNumOne = characters[Math.floor(Math.random()*characters.length)]
+        }
+        passWordOne+=(randNumOne)
+    }
+    return passWordOne
 }
 
 //illy
-
 function passWordGeneratorTwo(){
     let passWordTwo=""
     for (let i=0 ; i<15 ; i++){
-    let randomNumGenTwo=Math.floor(Math.random()*characters.length)
-    let randNumTwo=characters[randomNumGenTwo]
-    passWordTwo+=(randNumTwo)
+        let randomNumGenTwo=Math.floor(Math.random()*characters.length)
+        let randNumTwo=characters[randomNumGenTwo]
+        // condition paswd shouldn't start with - according to IBM
+        while(randNumTwo == '-'){
+            randNumTwo = characters[Math.floor(Math.random()*characters.length)]
+        }
+        passWordTwo+=(randNumTwo)
+    }
+    return passWordTwo
 }
-return passWordTwo
 
-}
 //log the gen pass when click the generate btn
 function paswrdGenBtn(){
-    
     outputOne.value=passWordGeneratorOne()
     outputTwo.value=passWordGeneratorTwo()
 }
 
-
 copyButtonOne.addEventListener('click', function(){
     navigator.clipboard.writeText(outputOne.value)
-
-
-    
 })
 
-copyButtonTwo.addEventListener( 'click ' ,function() {
-    copyButtonTwo.navigator.writeText(outputTwo.value)
-
+copyButtonTwo.addEventListener('click',function() {
+    navigator.clipboard.writeText(outputTwo.value)
+    console.log(outputTwo.value)
 })
 
+// caller function to change title color on set interval
+document.addEventListener('DOMContentLoaded', function() {
+    changingTitleColor()
+    setInterval(changingTitleColor, 1000);
+ });
 
-
+ // Function to change title color RGB
+ function changingTitleColor(){
+    let title = document.getElementById("title")
+    title.style.color = "#"+Math.floor(Math.random()*16777215).toString(16) // generate hex code for colors
+ }
